e62eef90d812e00a1e51dddd27eb57405068783b
Estecka <estecka@galacsys.com>
2018-12-10 16:01:33 +0100

    Added `Map` method for Arrays

---
+++ Assets/Estecka/Basics/Extensions/ArrayExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Estecka.Extensions {
+	static public class ArrayExtension {
+		/// <summary>
+		/// Process every items from this array and returns an array of the results.
+		/// </summary>
+		/// <param name="input">The array to preocess</param>
+		/// <param name="conversion">The function that will convert this array's elements</param>
+		/// <typeparam name="I">The source array's element type</typeparam>
+		/// <typeparam name="O">The destination array's element type</typeparam>
+		/// <returns></returns>
+		static public O[] Map<I, O>(this I[] input, System.Func<I, O> conversion){
+			O[] output = new O[input.Length];
+			for (int i=0; i<input.Length; i++)
+				output[i] = conversion.Invoke(input[i]);
+			return output;
+		}
+
+	} // END Extensions
+} // END Namespace
