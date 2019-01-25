0c47a77a4936c1a98e101c9884ed7e3c82947144
Estecka <estecka@galacsys.com>
2019-01-25 16:42:23 +0100

    `UnityScenes` will self-check their information at runtime upon deserialization. Removed the unused `UnityScene` constructor.

--- Assets/Estecka/Basics/Primitives/unityScene.cs
+++ Assets/Estecka/Basics/Primitives/unityScene.cs
@@ -5,17 +5,70 @@ using UnityEngine.SceneManagement;
 
 namespace Estecka {
 	[System.Serializable]
-	public struct UnityScene {
+	public struct UnityScene : ISerializationCallbackReceiver {
 		#if UNITY_EDITOR
 		public UnityEditor.SceneAsset sceneAsset;
 		#endif
 
 
 		public int buildIndex;
-		public UnityScene(int buildIndex){
-			this.buildIndex = buildIndex;
-			#if UNITY_EDITOR
-			sceneAsset = null;
+		public string path;
+
+		public void OnBeforeSerialize(){}
+		public void OnAfterDeserialize(){
+			if (Application.isPlaying)
+				this.RuntimeValidation();
+			else
+				this.EditorValidation();
+		}
+
+
+		public void RuntimeValidation(){
+			if (this.buildIndex < 0 && string.IsNullOrEmpty(this.path))
+				return;
+
+			int foundIndex = SceneUtility.GetBuildIndexByScenePath(this.path);
+			if (foundIndex < 0)
+			{
+				string foundPath = SceneUtility.GetScenePathByBuildIndex(this.buildIndex);
+				if (string.IsNullOrEmpty(foundPath)){
+					Debug.LogWarningFormat(
+						"No traces of this scene were found. \nPlease reassign this scene in the editor. \n" +
+						"Build Index : {0} \nPath : {1}", 
+						this.buildIndex, 
+						this.path
+					);
+					this.buildIndex = -1;
+					this.path = null;
+				} 
+				else {
+					Debug.LogWarningFormat(
+						"A scene path could not be found, its build index was found at a different path. \nMake sure the intended scene is assigned in the Editor. \n" +
+						"Build index : {0} \nExpected path : {1} \nActual path : {2}",
+						this.buildIndex,
+						this.path,
+						foundPath
+					);
+					this.path = foundPath;
+				}
+			}
+			else if (foundIndex != this.buildIndex)
+			{
+				Debug.LogWarningFormat(
+					"A scene was found with a different build index than expected \nMake sure the intended scene is assigned in the Editor \n" +
+					"Path : {0} \nExpected index : {1} \nActual index : {2}", 
+					this.path, 
+					this.buildIndex, 
+					foundIndex
+				);
+				this.buildIndex = foundIndex;
+			}
+		}
+
+		public void EditorValidation(){
+			#if !UNITY_EDITOR
+			Debug.LogError("EditorValidation will not work in builds.");
+			#else
 			#endif
 		}
 
